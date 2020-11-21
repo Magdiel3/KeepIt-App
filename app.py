284fde94306f685ff6f0f18f05850f06e4d792c9
Magdiel3 <magdieltercero@hotmail.com>
2020-11-21 16:01:12 -0600

    First commit

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-# from datetime import datetime
+from datetime import datetime
 
-# from bson.json_util import dumps
+from bson.json_util import dumps
 from flask import Flask, render_template, request, redirect, url_for
-# from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-# from flask_socketio import SocketIO, join_room, leave_room
-# from pymongo.errors import DuplicateKeyError
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_socketio import SocketIO, join_room, leave_room
+from pymongo.errors import DuplicateKeyError
 
 
 # Setup app with web socket and Login Handler
 app = Flask(__name__)
-# app.secret_key = "keepit"
-# socketio = SocketIO(app)
-# login_manager = LoginManager()
-# login_manager.login_view = 'login'
-# login_manager.init_app(app)
+app.secret_key = "keepit"
+socketio = SocketIO(app)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
 
 # Splash screen
