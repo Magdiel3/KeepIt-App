dcccb2c62b2b572be14ec8fb998aaf6d5a1a681e
Magdiel3 <magdieltercero@hotmail.com>
2020-11-21 18:55:29 -0600

    Added login

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 app.secret_key = "KeepItSecret"
 socketio = SocketIO(app)
 login_manager = LoginManager()
+login_manager.login_view = "login"
 login_manager.init_app(app)
 
 # Splash screen
@@ -37,11 +38,18 @@ def login():
 
     return render_template('login.html', message = message)
 
+# Logout
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
 # User screen
 @app.route('/box')
 def overview():
     username = request.args.get("username")
-    password = request.args.get("password")
+    password = request.args.get("box_id")
     
     if username and password:
         return render_template("home.thml",username=username, password=password)
